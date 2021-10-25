39eb1db9aa3b1d1cb15c119849c2752277a40b1c
Dheeraj R <83569971+DheerajR1@users.noreply.github.com>
2021-10-25 22:40:41 +0530

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Day4 {
     
     public static int lastStoneWeight(int[] stones) {
-		Arrays.sort(stones);
-		return -1;
+		List<Integer> data = new ArrayList<>();
+		for(int i =0; i<stones.length;i++) {
+			data.add(stones[i]);
+		}
 	}
     public static void main(String[] args) {
-		/******** DAY 4 ********/
+		/******** DAY 4 ********/     
 		
 		/******** DAY 4 ENDS ***/
 	}
-}
+ 
+}        
