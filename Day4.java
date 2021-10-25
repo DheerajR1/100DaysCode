8f119283e26f5f230464cf836f44e12807c7a2ad
Dheeraj R <83569971+DheerajR1@users.noreply.github.com>
2021-10-25 22:48:13 +0530

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -7,27 +7,29 @@ import java.util.Collections;
 import java.util.List;
 
 public class Day4 {
-    
-    public static int lastStoneWeight(int[] stones) {
+
+	public static int lastStoneWeight(int[] stones) {
 		List<Integer> data = new ArrayList<>();
-		for(int i =0; i<stones.length;i++) {
+		for (int i = 0; i < stones.length; i++) {
 			data.add(stones[i]);
 		}
-		while(data.size() > 1) {
+		while (data.size() > 1) {
 			Collections.sort(data);
-			if(data.get(data.size() -1) == data.get(data.size()-2)) {
-				data.remove(data.size()-1);
+			if (data.get(data.size() - 1) == data.get(data.size() - 2)) {
+				data.remove(data.size() - 1);
 			} else {
-				data.set(data.size()-2, data.get(data.size()-1) - data.get(data.size()-2));
+				data.set(data.size() - 2, data.get(data.size() - 1) - data.get(data.size() - 2));
 			}
-			data.remove(data.size() -1);
+			data.remove(data.size() - 1);
 		}
-		return data.size() == 1? data.get(0):0;
+		return data.size() == 1 ? data.get(0) : 0;
 	}
-    public static void main(String[] args) {
-		/******** DAY 4 ********/     
-		
+
+	public static void main(String[] args) {
+		/******** DAY 4 ********/
+		int[] num1 = { 2, 7, 4, 1, 8, 1 };
+		System.out.println(lastStoneWeight(num1));
 		/******** DAY 4 ENDS ***/
 	}
- 
-}        
+
+}
